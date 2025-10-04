1e88913004bd5e2f6f954ce2e04baf5970cd2091
aiquarks <aiquarkcrew@gmail.com>
2025-10-04 09:51:56 +0530

    Added comments and operations

--- IdeaProjects/StreamOperations/src/com/stream/example/AccountBalanceOps.java
+++ IdeaProjects/StreamOperations/src/com/stream/example/AccountBalanceOps.java
@@ -2,6 +2,8 @@ package com.stream.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class AccountBalanceOps {
     public static void main(String[] args) {
@@ -9,12 +11,14 @@ public class AccountBalanceOps {
         Account a4 = new Account("J", "C", 290.7f);
         Account a2 = new Account("B", "N", 60.7f);
         Account a3 = new Account("C", "K", 1900.7f);
+        Account a5 = new Account("B", "N", 60.7f);
 
         List<Account> accList = new ArrayList<>();
         accList.add(a1);
         accList.add(a2);
         accList.add(a3);
         accList.add(a4);
+        accList.add(a5);
 
         float total = totalAccBalByPerson(accList, "J", "C");
         System.out.println(" Total balance for J C: " + total);
@@ -24,8 +28,21 @@ public class AccountBalanceOps {
 
         float byName = sumOfBalanceByName(accList, "J", "C");
         System.out.println("Starts By Name : "+byName);
-    }
 
+        List<Account> distinctAccounts = listDistictfNamelNameCombination(accList);
+        System.out.println("Distinct Accounts (by Name): " + distinctAccounts.size());
+    }
+    /*
+     *  Filtering and Summing
+     */
+
+
+    /**
+     * @param accList
+     * @param fName
+     * @param lName
+     * @return
+     */
     private static float totalAccBalByPerson(List<Account> accList, String fName, String lName) {
         return (float)
                 accList.stream()
@@ -33,19 +50,56 @@ public class AccountBalanceOps {
                         .mapToDouble(Account::getBalance)
                         .sum();
     }
+
+    /**
+     * @param accList
+     * @return
+     */
     private static float  totalBalaceBelow1000(List<Account> accList){
         return (float) accList.stream().filter(e->e.getBalance()<1000).mapToDouble(Account::getBalance).sum();
     }
 
+    /**
+     * @param accList
+     * @param fName
+     * @param lName
+     * @return
+     */
     private static float sumOfBalanceByName(List<Account> accList, String fName, String lName){
         return (float) accList.stream().filter(e->e.getFirstName().startsWith("J") && e.getLastName().startsWith("J")).mapToDouble(Account::getBalance).sum();
     }
+
+    /*
+    Distinct and Grouping
+    */
+
+    /**
+     * @param accList
+     * @return
+     */
+    //List all distinct (firstName, lastName) combinations.
+    private static List<Account> listDistictfNamelNameCombination(List<Account> accList){
+        return accList.stream().distinct().collect(Collectors.toList());
+    }
+
 }
 
 class Account {
     private String firstName;
     private String lastName;
     private float balance;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Float.compare(balance, account.balance) == 0 && Objects.equals(firstName, account.firstName) && Objects.equals(lastName, account.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, balance);
+    }
 
     public Account(String firstName, String lastName, float balance) {
         this.firstName = firstName;
