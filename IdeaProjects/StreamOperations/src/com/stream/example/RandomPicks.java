231837b54e96b6fc11a2c0f500f5648e2de02b24
aiquarks <aiquarkcrew@gmail.com>
2025-10-09 11:36:54 +0530

    Added more  functionality

--- IdeaProjects/StreamOperations/src/com/stream/example/RandomPicks.java
+++ IdeaProjects/StreamOperations/src/com/stream/example/RandomPicks.java
@@ -11,8 +11,16 @@ public class RandomPicks {
         String input = "swiss";
         Character result = getFirstNonRepeatingCharacter(input);
 
+        Character lastCharacter = getLastNonRepeatingCharacter(input);
+
         if (result != null) {
             System.out.println("First non-repeating character in \"" + input + "\" is: " + result);
+        } else {
+            System.out.println("No non-repeating character found in \"" + input + "\"");
+        }
+
+        if (lastCharacter != null) {
+            System.out.println("First non-repeating character in \"" + input + "\" is: " + lastCharacter);
         } else {
             System.out.println("No non-repeating character found in \"" + input + "\"");
         }
@@ -32,5 +40,14 @@ public class RandomPicks {
                 .findFirst()
                 .orElse(null);
 
+    }
+    private static Character getLastNonRepeatingCharacter(String str){
+        return str.chars().mapToObj(c->(char)c).collect(
+                Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
+                .entrySet().stream()
+                .filter(e->e.getValue()==1)
+                .reduce((first,last)->last)
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 }
